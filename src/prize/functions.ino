1d4bd0a963d250857c3cc8db2380be181985e11d
Stephen Mann <steve@mann.ws>
2023-06-17 23:35:07 -0400

    Bumped start_button debounce to 300ms

--- src/prize/functions.ino
+++ src/prize/functions.ino
@@ -50,7 +50,7 @@ IRAM_ATTR void startButtonHandler() {
   static unsigned long last_interrupt_time = 0;
   unsigned long interrupt_time = millis();
   //Debounce
-  if (interrupt_time - last_interrupt_time > 250)
+  if (interrupt_time - last_interrupt_time > 300)
   {
     startButtonFlag = true;
   }
